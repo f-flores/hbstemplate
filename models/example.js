5a76f9d26c9f4ebee5cdabdd974efe2ff23ec554
Fabian Flores <ffflores1@outlook.com>
2019-06-11 23:12:19 -0400

    added comments to models/example.js

--- models/example.js
+++ models/example.js
@@ -1,3 +1,10 @@
+// ==================================================
+//
+// File name: example.js
+// Description: sequelize model example
+//
+// ==================================================
+
 module.exports = function(sequelize, DataTypes) {
   var Example = sequelize.define("Example", {
     text: DataTypes.STRING,
